c1324f647284ed50ac7b618939bbfe5a0731d126
Миша К <mihailkononov25256@gmail.com>
2026-09-11 10:36:11 +0500

    fix(seeder): вынесены лишние классы

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,12 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('admin')
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
