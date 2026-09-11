14854f5862525e033a557b32394b52fa5aed644b
Миша К <mihailkononov25256@gmail.com>
2026-09-11 10:26:03 +0500

    feat: сидер с добавлением базового набора ролей

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Создание базового набора ролей
+     */
+    public function run(): void
+    {
+        Role::create(['role_name' => 'admin']);
+        Role::create(['role_name' => 'guest']);
+    }
+}
