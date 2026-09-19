e7d98bf6a9f47690136aa9eef9c296c9387b2986
Миша К <mihailkononov25256@gmail.com>
2026-09-19 14:27:43 +0500

    fix(Controller): упущенный extend от BaseController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests;
 }
