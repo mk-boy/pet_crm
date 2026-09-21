06f371366943f950b04fa3139ff30adff4eaa9fa
Миша К <mihailkononov25256@gmail.com>
2026-09-21 23:24:04 +0500

    feat(locale): добавлена локализация новых фраз для формы редактирования пользователя

--- lang/en/menu.php
+++ lang/en/menu.php
@@ -5,6 +5,7 @@ return [
     'dashboard' => 'Dashboard',
     'users' => 'Users',
     'users_create' => 'Create User',
+    'users_edit' => 'Edit User',
     'profile' => 'Profile',
     'account' => 'Account',
     'logout' => 'Log Out',
