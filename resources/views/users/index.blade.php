27f1a10b42df64e4946818f0a343fdb971f4f832
Миша К <mihailkononov25256@gmail.com>
2026-09-22 17:06:48 +0500

    feat(trash-user): добавлена кнопка удаления пользователя на users.index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -48,16 +48,27 @@
                         @endif
                     </td>
                     <td class="w-px text-end">
-                        @can('update', $user)
-                            <a
-                                href="{{ route('users.edit', $user) }}"
-                                class="inline-flex size-11 items-center justify-center rounded-lg text-label-2 transition-colors hover:bg-label/5 hover:text-label active:bg-label/10 motion-reduce:transition-none sm:size-9"
-                                aria-label="{{ __('users.edit', ['name' => $user->name]) }}"
-                                title="{{ __('users.edit', ['name' => $user->name]) }}"
+                        <div class="flex items-center justify-end gap-1">
+                            @can('update', $user)
+                                <a
+                                    href="{{ route('users.edit', $user) }}"
+                                    class="inline-flex size-11 items-center justify-center rounded-lg text-label-2 transition-colors hover:bg-label/5 hover:text-label active:bg-label/10 motion-reduce:transition-none sm:size-9"
+                                    aria-label="{{ __('users.edit', ['name' => $user->name]) }}"
+                                    title="{{ __('users.edit', ['name' => $user->name]) }}"
+                                >
+                                    <x-icons.pencil />
+                                </a>
+                            @endcan
+
+                            <button
+                                type="button"
+                                class="inline-flex size-11 items-center justify-center rounded-lg text-label-2 transition-colors hover:bg-danger/10 hover:text-danger active:bg-danger/15 motion-reduce:transition-none sm:size-9"
+                                aria-label="{{ __('users.delete', ['name' => $user->name]) }}"
+                                title="{{ __('users.delete', ['name' => $user->name]) }}"
                             >
-                                <x-icons.pencil />
-                            </a>
-                        @endcan
+                                <x-icons.trash />
+                            </button>
+                        </div>
                     </td>
                 </tr>
             @endforeach
