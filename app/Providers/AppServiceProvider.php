764144d5fd4038740e097dcb3648e623adabe643
Миша К <mihailkononov25256@gmail.com>
2026-09-23 03:48:31 +0500

    fix(dashboard-gate): правило изменено на 'есть хотя бы какая то роль у пользователя'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('access-dashboard', function (User $user) {
-            return $user->role?->role_name === 'admin'
-                || $user->role?->role_name === 'guest';
-        });
+        Gate::define('access-dashboard', fn (User $user) => $user->role !== null);
     }
 }
