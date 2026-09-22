1f350fe1db136f0b2c5dd53ce0bb6a0a594e2dc6
Миша К <mihailkononov25256@gmail.com>
2026-09-22 12:27:26 +0500

    fix(main-page-redirect): основной роут теперь сразу редиректит на дашборд

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/dashboard', function () {
